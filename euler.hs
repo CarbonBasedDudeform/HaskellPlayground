4b2d1f68157974d6776de5ba16e5c726ad9e0054
Dom Musgrave <imdomdude@gmail.com>
2015-02-25 17:45:28 +0000

    problem 2 solution
    solution to project euler problem 2, not the best solution though.

--- euler.hs
+++ euler.hs
@@ -6,3 +6,16 @@ sumMultiples3or5 (x:xs)
     | x `mod` 5 == 0 = x + sumMultiples3or5 xs
     | otherwise      = sumMultiples3or5 xs
 
+
+fib :: Int -> Int
+fib 1 = 1
+fib 2 = 2
+fib x = fib(x-1) + fib(x-2)
+
+--evenValuesInFibonacci [1..33] for problem answer
+evenValuesInFibonacci :: [Int] -> Int
+evenValuesInFibonacci [] = 0
+evenValuesInFibonacci [x] = 0
+evenValuesInFibonacci (x:xs)
+    | (fib x) `mod` 2 == 0 = fib x + evenValuesInFibonacci xs
+    | otherwise = evenValuesInFibonacci xs
