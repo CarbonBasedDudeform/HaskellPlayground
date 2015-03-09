928908c26fcb6b1021d904d30144aa4d7d01ef1d
Dom Musgrave <imdomdude@gmail.com>
2015-03-09 00:50:03 +0000

    Hello World
    had to be done

--- double.hs
+++ double.hs
@@ -1,3 +1,5 @@
+import System.IO
+
 double x = x +x
 quadruple x = double (double x)
 factorial n = product [1..n]
@@ -12,3 +14,18 @@ mylast xs = take 1 (reverse xs)
 
 add' :: Int -> (Int -> Int)
 add' x y = x + y
+
+pyths :: Int -> [(Int, Int, Int)]
+pyths n = [(x, y, z) | x <- [1..n],y <- [1..n],z <- [1..n],x^2 + y^2 == z^2  ]
+
+factors n y = [x | x <- [1..y], x `mod` n == 0]
+
+helloWorld :: IO ()
+helloWorld = putStrLn "Hello, world!"
+
+welcome :: String -> IO()
+welcome name = do
+    putStr ("Hello, ")
+    putStr (name)
+    putStr (", welcome")
+    putStrLn ""
