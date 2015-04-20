32169e1421cb095f2133af7f1c6293396a4e4358
dom <contact@dominicmusgrave.co.uk>
2015-04-20 21:09:03 +0100

    Some semblance of story

--- textAdventure.hs
+++ textAdventure.hs
@@ -1,17 +1,29 @@
 import System.IO
 
+--Data types used throughout
 fullHealth = 1.0
-
-main = do
-  playersName <- greet
-  let p = Player playersName fullHealth
-  playGame p
-  farewell
+fullStrength = 1.0
 
 data Player = Player {
 	name :: String,
-	health :: Float
+	health :: Float,
+  strength :: Float
 } deriving (Show)
+
+--props prefixed with e due to
+-- http://stackoverflow.com/questions/24352280/haskell-multiple-declarations-of-x
+data Enemy = Enemy {
+  eName :: String,
+  eHealth :: Float,
+  eStrength :: Float
+} deriving (Show)
+
+--main functions
+main = do
+  playersName <- greet
+  let p = Player playersName fullHealth fullStrength
+  playGame p
+  farewell
 
 greet = do
   putStrLn "What is your name?"
@@ -23,8 +35,14 @@ askName = do
   name <- getLine
   return name
 
+levelOne player enemy = do
+  putStrLn ( "Oh shit " ++ (show $ player) ++ " walks into a bar.")
+  putStrLn ( "Has a few beers and pisses off")
+  putStrLn (show $ enemy)
+
 playGame player = do
-	putStrLn ( "Unimplemented " ++ (show $ player) )
+  let boss = Enemy "Bob" 1.0 1.0
+  levelOne player boss
 
 farewell = do
 	putStrLn "Thanks for playing"
