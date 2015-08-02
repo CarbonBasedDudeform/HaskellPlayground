2790031bf959c0590b76ee34c2082cc610550fc8
dom <contact@dominicmusgrave.co.uk>
2015-08-02 15:54:18 +0100

    Solution(s) to list problems

--- roadtologic/jusPlayingWithLists.hs
+++ roadtologic/jusPlayingWithLists.hs
@@ -1,3 +1,9 @@
 reverseList :: [a] -> [a]
 reverseList [] = []
 reverseList (x:xs) = (reverseList xs) ++ [x]
+
+splitList :: [a] -> [([a], [a])]
+splitList [x,y] = [([x], [y])]
+splitList (x:y:zs) = ([x], (y:zs)) : addLeft x (splitList (y:zs))
+  where addLeft u [] = []
+        addLeft u ((vs, ws): rest) = (u:vs, ws) : addLeft u rest
