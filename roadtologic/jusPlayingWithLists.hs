84aa3be44c0c6d67bf5c29c592a105c09d247e24
dom <contact@dominicmusgrave.co.uk>
2015-08-03 00:02:35 +0100

    functions and that

--- roadtologic/jusPlayingWithLists.hs
+++ roadtologic/jusPlayingWithLists.hs
@@ -7,3 +7,18 @@ splitList [x,y] = [([x], [y])]
 splitList (x:y:zs) = ([x], (y:zs)) : addLeft x (splitList (y:zs))
   where addLeft u [] = []
         addLeft u ((vs, ws): rest) = (u:vs, ws) : addLeft u rest
+
+divisors :: Integer -> [(Integer,Integer)]
+divisors n = [(d, quot n d) | d <- [1..k], rem n d == 0]
+  where k = floor (sqrt (fromInteger n))
+
+
+prime' :: Integer -> Bool
+prime' = \n -> divisors n == [(1, n)]
+
+
+--recurrence form of factorial function
+fac 0 = 1
+fac n = fac (n-1) * n
+
+fac' n = product [x | x <- [1..n]]
